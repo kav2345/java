1ca73b87a72abf3e5b97daab4b5a7ae1efcdacc2
kav2345 <22hr1a3250@mtieat.org>
2024-12-12 15:22:03 +0530

    Create scp.java

---
+++ scp.java
@@ -0,0 +1,20 @@
+***
+heap and scp string ..
+[reference based]
+output:
+springfall
+springwinter
+***
+public class str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1=new String("spring");
+		str1.concat ("summer");
+		String str2=str1.concat("winter");
+		str1=str1.concat("fall");
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+
+}
